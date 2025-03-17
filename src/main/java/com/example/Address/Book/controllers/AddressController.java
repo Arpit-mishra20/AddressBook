1e7811609660a8f71b61f84bbd18cb5385dc63ed
Arpit-mishra20 <arpit.202033@gmail.com>
2025-03-17 10:49:13 +0530

    Handling REST API using service layer with storage in database

--- src/main/java/com/example/Address/Book/controllers/AddressController.java
+++ src/main/java/com/example/Address/Book/controllers/AddressController.java
@@ -14,59 +14,6 @@ public class AddressController {
     @Autowired
     IEmployeeService iEmployeeService;
 
-    //UC1 --> REST API's handled using responseDTO(without interference of service layer)
-
-    @GetMapping("/res/get/{id}")
-    public ResponseDTO get1(@PathVariable Long id){
-        return new ResponseDTO("API triggered at /res/get/{id}", "Success");
-    }
-
-    @PostMapping("/res/create")
-    public ResponseDTO create1(@RequestBody EmployeeDTO user){
-        return new ResponseDTO("API triggered at /res/create", "Success");
-    }
-
-    @GetMapping("/res/getAll")
-    public ResponseDTO getAll1(){
-        return new ResponseDTO("API triggered at /res/getAll", "Success");
-    }
-
-    @PutMapping("/res/edit/{id}")
-    public ResponseDTO edit1(@RequestBody EmployeeDTO user, @PathVariable Long id){
-        return new ResponseDTO("API triggered at /res/edit/{id}", "Success");
-    }
-
-    @DeleteMapping("/res/delete/{id}")
-    public ResponseDTO delete1(@PathVariable Long id){
-        return new ResponseDTO("API triggered at /res/delete/{id}", "Success");
-    }
-
-    //UC2 --> Handling REST API's using Service layer without storing in DB
-
-    @GetMapping("/res2/get/{id}")
-    public ResponseDTO get2(@PathVariable Long id){
-        return iEmployeeService.response("API triggered at /res/get/{id}", "Success");
-    }
-
-    @PostMapping("/res2/create")
-    public ResponseDTO create2(@RequestBody EmployeeDTO user){
-        return iEmployeeService.response("API triggered at /res/create", "Success");
-    }
-
-    @GetMapping("/res2/getAll")
-    public ResponseDTO getAll2(){
-        return iEmployeeService.response("API triggered at /res/getAll", "Success");
-    }
-
-    @PutMapping("/res2/edit/{id}")
-    public ResponseDTO edit2(@RequestBody EmployeeDTO user, @PathVariable Long id){
-        return iEmployeeService.response("API triggered at /res/edit/{id}", "Success");
-    }
-
-    @DeleteMapping("/res2/delete/{id}")
-    public ResponseDTO delete2(@PathVariable Long id){
-        return iEmployeeService.response("API triggered at /res/delete/{id}", "Success");
-    }
 
     //UC3 --> Handling REST API's using service layer with storage in database
 
